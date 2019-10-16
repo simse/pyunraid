684141e9fd80fb3b5b1c8ccf53628a9214ce5ce0
BuildTools <unconfigured@null.spigotmc.org>
2019-10-16 16:06:27 +0100

    Added plugins

--- pyunraid/models/plugin.py
+++ pyunraid/models/plugin.py
@@ -8,6 +8,7 @@ class Plugin():
         self.support_thread = ''
         self.update_status = ''
         self.version = ''
+        self.unraid = None
 
 
     def uninstall(self):
@@ -16,3 +17,4 @@ class Plugin():
 
     def release_notes(self):
         pass
+        #return self.unraid.get("/plugins/dynamix.plugin.manager/include/ShowChanges.php?file=%2Ftmp%2Fplugins%2F{}.txt".format(self.name)).text
--- pyunraid/plugins.py
+++ pyunraid/plugins.py
@@ -8,22 +8,54 @@ from pyunraid.constants import *
 from pyunraid.models.plugin import Plugin
 
 
-def plugins(u):
+PLUGIN_UPDATE_STATUS = {
+    'up-to-date': 'UP_TO_DATE'
+}
 
+
+def plugins(u):
     return parse_plugins(u)
 
 
 def parse_plugins(u):
     # Parse containers page
     soup = BeautifulSoup(u.get('/plugins/dynamix.plugin.manager/include/ShowPlugins.php?check=0').text, 'lxml')
+    #soup = BeautifulSoup(open('html/plugins.html', 'r'), 'lxml')
     plugins = []
 
     for plugin in soup.find_all('tr'):
         p = Plugin()
 
-        # Find plugin name
-        p.name = plugin.select('strong')[0].text
+        # Find plugin image
+        if plugin.find('img'):
+            p.image = plugin.find('img')['src']
 
+        else:
+            p.image = 'icon:' + plugin.find('i')['class'][1]
+
+        # Find plugin name and description
+        if plugin.select('.desc_readmore h4'):
+            p.name = plugin.select('.desc_readmore h4')[0].text
+            p.description = plugin.select('.desc_readmore p')[0].text
+
+        else:
+            p.name = plugin.select('.desc_readmore strong')[0].text
+            p.description = plugin.select('.desc_readmore p')[1].text
+
+
+        # Find plugin support thread
+        p.support_thread = plugin.find_all('a')[1]['href']
+
+        # Find plugin author
+        p.author = plugin.find_all('td')[2].text
+
+        # Find plugin version
+        p.version = plugin.find_all('td')[3].text
+
+        # Find plugin update status
+        p.update_status = PLUGIN_UPDATE_STATUS[plugin.find_all('td')[4].text]
+
+        p.unraid = u
 
         plugins.append(p)
 
