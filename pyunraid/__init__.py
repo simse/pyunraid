8d3d24eb41412e59426608f02b3f026b2132eace
Simon Nørager Sørensen <6843486+simse@users.noreply.github.com>
2019-10-17 10:53:43 +0100

    Test fix

---
+++ pyunraid/__init__.py
@@ -0,0 +1,6 @@
+from pyunraid.models.container import Container
+from pyunraid.models.disk import Disk
+from pyunraid.models.plugin import Plugin
+from pyunraid.models.share import Share
+from pyunraid.models.user import User
+from pyunraid.models.vm import VM
