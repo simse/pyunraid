9e152d6addbf96804430b73eee9d38f6f258c21b
Simon Sørensen <hello@simse.io>
2019-11-04 17:28:18 +0000

    Update build.sh

--- web/build.sh
+++ web/build.sh
@@ -2,7 +2,8 @@
 pip install -U sphinx
 pip install -e ../.
 make -C ../docs html
-mkdir public/docs
-cp -r ../docs/_build/html public/docs
 npm install
 brunch build --production
+mkdir public
+mkdir public/docs
+cp -r ../docs/_build/html public/docs
